202b0808c343458b5118297d73450060ec2d86b6
airin01 <155235361+airin01@users.noreply.github.com>
2024-01-07 07:10:57 +0600

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,3 @@
-# hello_world.py
 print("Hello, world!")
-# Change in main to hello_world.py line 2
-# Change in feature/modify-print to hello_world.py line 3
+print("Hello darkness my old friend")
+print("I've come to talk with you again")
